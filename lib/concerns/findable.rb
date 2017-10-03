16137f550a3c8cba9133df19ce4cf46287fb89f2
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-03 02:55:58 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,8 +1,9 @@
 module findable
-
-  def find_by_name(name)
-    self.all.detect{|a| a.name == name}
+  
+  module ClassMethods
+    def find_by_name(name)
+      self.all.detect{|a| a.name == name}
+    end
   end
-
   
 end
